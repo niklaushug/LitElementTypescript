44dcc10a69d2ea09e451dbb74bf65b2e0aebbfcb
Niklaus Hug <biz@niklaushug.ch>
2019-11-04 12:55:05 +0100

    demonstate how to pass functions from one lit-element to another

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,44 @@
-import {
-  LitElement, html, customElement, property
-} from 'lit-element';
+import { LitElement, html, customElement, property } from 'lit-element';
 
-@customElement('my-element')
-export class MyElement extends LitElement {
+@customElement('nhug-button')
+export class InnerElement extends LitElement {
+  @property({
+    type : String
+  }) label = ''
+  @property({
+    type : Object
+  }) onClickHandler = () => {console.log('clickHandler of nhug-button')};
 
-  @property()
-  foo = 'foo';
+  render() {
+    return html`
+       <button @click="${this.onClickHandler}">${this.label}</button>
+    `;
+  }
+}
 
-  render(){
-    return html`<p>${this.foo}</p>`;
+@customElement('nhug-button-group')
+export class OuterElement extends LitElement {
+
+  clickHandler() {
+    console.log('clickHandler of nhug-button-group');
+  }
+
+  render() {
+    return html`
+      <div class="nhug-button-group">
+        <button @click="${this.clickHandler}">nhug-button-group executes clickHandler of nhug-button-group</button>
+        <nhug-button
+          label="nhug-button executes clickHandler of nhug-button-group"
+          .onClickHandler="${this.clickHandler}"
+        ></nhug-button>
+        <nhug-button
+          label="nhug-button executes executes passed in function"
+          .onClickHandler="${() => console.log('passed in function')}"
+        ></nhug-button>
+        <nhug-button
+          label="nhug-button executes executes clickHandler of nhug-button"
+        ></nhug-button>
+      </div>
+    `;
   }
 }
